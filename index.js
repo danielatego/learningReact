5c02b1193dd5a779b39a2baa2f5a5b14a157683f
danielatego <danielatego75@gmail.com>
2023-04-26 13:03:19 +0300

    using query selector

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-ReactDOM.render(<h1>Hello1, everyopne!</h1>,document.getElementById("root"))
+// ReactDOM.render(<h1>Hello1, everyopne!</h1>,document.getElementById("root"))
+ReactDOM.render(<h1>Hello1, everyone!</h1>,document.querySelector("#root"))
+
