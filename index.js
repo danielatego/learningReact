fdbcb1db8eb26fe6ac00bd6c49d42cb8aefcf769
danielatego <danielatego75@gmail.com>
2023-04-27 11:51:01 +0300

    using function with reactdom

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
+function MainContent(){
+    return(
+    <gulag>
+        <h1 className="pb-3">God in heaven is Great</h1>
+        <p>lets worship him for He is our creator
+            and He is holy.
+        </p>
+
+    </gulag>
+    )
+}
 ReactDOM.render(
-    <ol>
-        <li> Julia </li>
-        <li> Crystal </li>
-        <li> Caro </li>
-    </ol>
-    ,document.querySelector('#root')
+    <div>
+        <Navbar/>
+        <MainContent/>
+    </div>,
+    document.getElementById('root')
+)
+ReactDOM.render(
+    <MainContent/>,
+    document.getElementById('waroota')
 )
