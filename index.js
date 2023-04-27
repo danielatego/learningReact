56a7ccd92ff079dc3b1a9512450565bd1a75e637
danielatego <danielatego75@gmail.com>
2023-04-27 10:54:47 +0300

    ordered list using reactdom.render

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// ReactDOM.render(<h1>Hello1, everyopne!</h1>,document.getElementById("root"))
-ReactDOM.render(<h1>Hello1, everyone!</h1>,document.querySelector("#root"))
-
+ReactDOM.render(
+    <ol>
+        <li> Julia </li>
+        <li> Crystal </li>
+        <li> Caro </li>
+    </ol>
+    ,document.querySelector('#root')
+)
