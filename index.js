438567a794996e66ab4702bdae0c5f50b71c2b89
danielatego <danielatego75@gmail.com>
2023-04-27 13:31:56 +0300

    jsx

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function gulab(){
-    //v=document.getElementById('root')
-    const love = document.createElement('h1')
-    love.textContent='I love to go to church'
-    love.className = 'header'
-    //const node =document.createTextNode('I love to go to church')
-    document.getElementById('root').append(love)
-    //love.appendChild(node);
-    //v.appendChild(love);
-}
+// function gulab(){
+//     //v=document.getElementById('root')
+//     //const node =document.createTextNode('I love to go to church')
+//     document.getElementById('root').append(love)
+//     //love.appendChild(node);
+//     //v.appendChild(love);
+// }
+const love = document.createElement('h1')
+love.textContent='I love to go to church'
+love.className = 'header'
+console.log(love)
+
+const element  = <h1 className = "mt-5">This is JSX.</h1>
+console.log(element);
+ReactDOM.render(element,document.querySelector('#root'))
